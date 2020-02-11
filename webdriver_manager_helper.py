019a7a7e1ef3bb3734d859aa86678aa96f7ec309
Jarosław Sokolik <44551668+JaroslawSokolik@users.noreply.github.com>
2020-02-11 20:58:14 +0100

    url changed to python.org

--- webdriver_manager_helper.py
+++ webdriver_manager_helper.py
@@ -3,5 +3,5 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
-driver.get("https://www.udemy.com/course/kurs-selenium-python/learn/lecture/14519262#overview")
+driver.get("https://www.python.org/")
 driver.close()
