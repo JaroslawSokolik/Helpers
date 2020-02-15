73d1ed53cb7658395fdd1ce10fa705d2dc3a4d48
Jarosław Sokolik <44551668+JaroslawSokolik@users.noreply.github.com>
2020-02-15 20:36:53 +0100

    python helpers added

---
+++ Python/if_else.py
@@ -0,0 +1,11 @@
+# Tylko jeden z bloków zostanie odpalony
+
+name = "Jarek"
+age = 3000
+
+if name == 'Alicja':
+    print ('Hi, Alicja')
+elif age < 12:
+    print ('You are not Jarek')
+elif age > 2000:
+    print ('You are Jarek') #this statement is True so it executes in this block
