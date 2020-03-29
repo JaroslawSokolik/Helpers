1557f43c102b4fb76393baff028f9cf2e3a12878
jarek <jaroslaw.sokolik@clicktrans.pl>
2020-03-29 21:11:32 +0200

    nauka

--- Python/data_types.py
+++ Python/data_types.py
@@ -1,3 +1,16 @@
-#List allows multiple data types and values
+#List
+# allows multiple data types and values
 
-values = [1, 2, 3, "Jarek", "maciej", 4]
+value = [1, 2, 3, "Jarek", "maciej", 4]
+
+print(value[0])
+print(value[1])
+print(value[2])
+print(value[3])
+print(value[-1])
+print(value[0:3])
+
+#Dictionary
+
+dic = {"a":"Hello World", "b": "Hello Poland"}
+print(dic["a"])
