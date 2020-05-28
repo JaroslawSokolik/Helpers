07e62256f0d78df1ed700ff3584262f04673a72a
jarek <misiek696969>
2020-05-28 21:50:10 +0200

    nauka

---
+++ Pytest/test_1.py
@@ -0,0 +1,21 @@
+# All pytest file must start with test_ name or at the end _test
+
+# it must be in functions!
+
+# function bame must be test_
+
+# to run from console should open specific folder in console and write py.test to run all
+
+def test_demo_1():
+    print("hello")
+
+def test_demo_2():
+    print("hello")
+
+def test_demo_3():
+    print("hello")
+
+def test_demo_4():
+    print("hello")
+
+
