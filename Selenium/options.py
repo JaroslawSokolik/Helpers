1211d5e53cc7e3390b575af584a299ae4cfad7e3
jarek <misiek696969>
2020-05-28 21:06:42 +0200

    nauka

---
+++ Selenium/options.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--start-maximized")
+# chrome_options.add_argument("headless")
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.biznesradar.pl/")
+print(driver.title)
