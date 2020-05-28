a9f41d6cfabdcd0429d130d5b78865c561059c61
jarek <misiek696969>
2020-05-28 13:29:18 +0200

    nauka

--- Selenium/waits.py
+++ Selenium/waits.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
 from time import sleep
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 driver = webdriver.Chrome()
+# driver.implicitly_wait(20) to wait for 20 sec with trying every all the time
 driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
 driver.maximize_window()
 driver.find_element_by_css_selector("input.search-keyword").send_keys("ber")
@@ -15,6 +20,9 @@ for button in buttons:
 
 driver.find_element_by_css_selector("img[alt='Cart']").click()
 driver.find_element_by_xpath("//div[@class='action-block']/button").click()
-driver.find_element_by_css_selector("input[class='promoCode']").send_keys("rahulshettyacademy")
-sleep(5)
+wait = WebDriverWait(driver, 5)
+wait.until(EC.presence_of_element_located((By.CLASS_NAME, "promoCode")))
+driver.find_element_by_class_name("promoCode").send_keys("rahulshettyacademy")
 driver.find_element_by_class_name("promoBtn").click()
+sleep(3)
+print(driver.find_element_by_class_name("promoInfo").text)
