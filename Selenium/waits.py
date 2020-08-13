34a4ae6a882f5e215a8d6347abf4adb1e36756a8
jarek <jaroslaw.sokolik@clicktrans.pl>
2020-08-13 17:33:17 +0200

    nauka

--- Selenium/waits.py
+++ Selenium/waits.py
@@ -11,3 +11,12 @@ ActionChains(driver).move_to_element(men_menu).perform()
 # wait for Fastrack menu item to appear, then click it
 fastrack = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, "//a[@data-tracking-id='0_Fastrack']")))
 fastrack.click()
+#try few times
+
+for x in range(0, 4):  # try 4 times
+    try:
+        element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "wait-for-offer")))
+        assert element.is_displayed()
+
+    except:
+        sleep(2)
