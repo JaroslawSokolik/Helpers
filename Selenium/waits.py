d51e46a22c522ba3735755ec505459df71cdbd82
jarek <jaroslaw.sokolik@clicktrans.pl>
2020-08-13 11:29:20 +0200

    nauka

--- Selenium/waits.py
+++ Selenium/waits.py
@@ -1,28 +1,13 @@
-from selenium import webdriver
-from time import sleep
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome()
-# driver.implicitly_wait(20) to wait for 20 sec with trying every all the time
-driver.get("https://rahulshettyacademy.com/seleniumPractise/#/")
-driver.maximize_window()
-driver.find_element_by_css_selector("input.search-keyword").send_keys("ber")
-sleep(4)
-count = len(driver.find_elements_by_xpath("//div[@class='products']/div"))
-assert count == 3
 
-buttons = driver.find_elements_by_xpath("//div[@class='product-action']/button")
-for button in buttons:
-    button.click()
 
-driver.find_element_by_css_selector("img[alt='Cart']").click()
-driver.find_element_by_xpath("//div[@class='action-block']/button").click()
-wait = WebDriverWait(driver, 5)
-wait.until(EC.presence_of_element_located((By.CLASS_NAME, "promoCode")))
-driver.find_element_by_class_name("promoCode").send_keys("rahulshettyacademy")
-driver.find_element_by_class_name("promoBtn").click()
-sleep(3)
-print(driver.find_element_by_class_name("promoInfo").text)
+# wait for element to appear, then hover it
+wait = WebDriverWait(driver, 10)
+men_menu = wait.until(ec.visibility_of_element_located((By.XPATH, "//a[@data-tracking-id='men']")))
+ActionChains(driver).move_to_element(men_menu).perform()
+
+# wait for Fastrack menu item to appear, then click it
+fastrack = WebDriverWait(driver, 10).until(ec.visibility_of_element_located((By.XPATH, "//a[@data-tracking-id='0_Fastrack']")))
+fastrack.click()
