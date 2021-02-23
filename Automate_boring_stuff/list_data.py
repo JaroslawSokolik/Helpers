bd295800d654259d15092989617f430d920735bb
jarek <jaroslaw.sokolik@clicktrans.pl>
2021-02-23 11:51:48 +0100

    list data

---
+++ Automate_boring_stuff/list_data.py
@@ -0,0 +1,11 @@
+#list
+
+animals = ["cat", "bat", "rat", "elephant", "dog", "giraffe"]
+
+#integer index to access item in the list
+
+print(animals[1])
+print(animals[-1])
+print(animals[1:3])
+
+assert "cat" in animals
