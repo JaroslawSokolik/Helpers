7385a0103a55ca2d5c23f9af5972a1cd34b700ea
jarek <jaroslaw.sokolik@clicktrans.pl>
2021-02-23 12:06:48 +0100

    for loop

---
+++ Automate_boring_stuff/for_loops.py
@@ -0,0 +1,11 @@
+colours = ["black", "red", "pink", "orange", "blue"]
+
+#check the index of item in the list
+
+for i in range(len(colours)):
+    print("Index " + str(i) + " in colours is " + colours[i])
+
+#print each item in the list
+
+for color in colours:
+    print(color)
